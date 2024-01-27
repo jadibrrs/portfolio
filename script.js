a6f7b36cd0e940c26be09a145bc2a4b6585d6401
Jadi Barros Heliodoro <jbheliodoro@gmail.com>
2024-01-27 02:52:29 -0400

    melhoria em botões, renomeação de arquivo, reorganização das formações

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
-let sectionQa = document.getElementById("elemento-pai-qa");
+function toggleContent(elementId, content) {
+  const element = document.getElementById(elementId);
 
-function mostrarQa(event){
-  event.preventDefault();
-  document.getElementById("elemento-pai-qa").innerHTML = `
+  if (element.style.display === "none" || !element.innerHTML.trim()) {
+    element.innerHTML = content;
+    element.style.display = 'block';
+  } else {
+    element.innerHTML = '';
+    element.style.display = 'none';
+  }
+}
+
+function mostrarQa(event) {
+  event.preventDefault()
+  toggleContent("elemento-pai-qa", `
   <ul>
     <li>Introdução à Qualidade</li>
     <li>Cultura da Qualidade</li>
@@ -12,42 +22,38 @@ function mostrarQa(event){
     <li>MongoDB</li>
     <li>Postman</li>
   </ul>
-  `
-};
+  `);
+}
 
-function mostrarProz(event){
-  event.preventDefault();
-  document.getElementById("elemento-pai-proz").innerHTML = `
-  <ul>
-    <li>Lógica de Programação</li>
-    <li>Git e GitHub</li>
-    <li>HTML</li>
-    <li>CSS</li>
-    <li>JavaScript</li>
-    <li>Banco de Dados</li>
-  </ul>
-  `
-};
+function mostrarProz(event) {
+  event.preventDefault()
+  toggleContent("elemento-pai-proz", `
+    <ul>
+      <li>Lógica de Programação</li>
+      <li>Git e GitHub</li>
+      <li>HTML</li>
+      <li>CSS</li>
+      <li>JavaScript</li>
+      <li>Banco de Dados</li>
+    </ul>
+  `);
+}
 
-function mostrarAtlantico(event){
-  event.preventDefault();
-  document.getElementById("elemento-pai-atlantico").innerHTML = `
-  <ul>
-    <li>Fundamentos de Qualidade e Teste de Software</li>
-    <li>Tipos e Níveis de Testes/Pirâmides de Testes</li>
-    <li>Estratégia e Técnicas de testes</li>
-    <li>Processo Fundamental de Teste</li>
-    <li>Processo de Teste Ágil</li>
-    <li>Introdução à Automação de Testes</li>
-    <li>Automação de Testes Funcionais</li>
-    <li>Automação de Testes de API</li>
-  </ul>
-  `
-};
+function mostrarAtlantico(event) {
+  event.preventDefault()
+  toggleContent("elemento-pai-atlantico", `
+    <ul>
+      <li>Fundamentos de Qualidade e Teste de Software</li>
+      <li>Tipos e Níveis de Testes/Pirâmides de Testes</li>
+      <li>Estratégia e Técnicas de testes</li>
+      <!-- Adicione mais itens conforme necessário -->
+    </ul>
+  `);
+}
 
-function mostrarEstagio(event){
-  event.preventDefault();
-  document.getElementById("elemento-pai-estagio").innerHTML = `
+function mostrarEstagio(event) {
+  event.preventDefault()
+  toggleContent("elemento-pai-estagio", `
   <ul>
     <li>Realização de testes funcionais manuais em sistemas web</li>
     <li>Realização de testes de regressão e integração</li>
@@ -59,12 +65,12 @@ function mostrarEstagio(event){
     <li>Colaboração com desenvolvedores para ajustes e explicação de tarefas</li>
     <li>Participação em reuniões de equipe para discussão de progresso e metas</li>
   </ul>
-  `
-};
+  `);
+}
 
-function mostrarJiraConfluence(event){
-  event.preventDefault();
-  document.getElementById("elemento-pai-jira-confluence").innerHTML = `
+function mostrarJiraConfluence(event) {
+  event.preventDefault()
+  toggleContent("elemento-pai-jira-confluence", `
   <ul>
     <li>Jira para times ágeis</li>
     <li>Scrum e Kanban no Jira</li>
@@ -79,12 +85,12 @@ function mostrarJiraConfluence(event){
     <li>Começando com Confluence</li>
     <li>Utilizando Jira com Confluence</li>
   </ul>
-  `
-};
+  `);
+}
 
-function mostrarTAT(event){
-  event.preventDefault();
-  document.getElementById("elemento-pai-tat").innerHTML = `
+function mostrarTAT(event) {
+  event.preventDefault()
+  toggleContent("elemento-pai-tat", `
   <ul>
     <li>Configurar um projeto Cypress do zero</li>
     <li>Visitar páginas locais e remotas</li>
@@ -98,17 +104,14 @@ function mostrarTAT(event){
     <li>Executar os testes em um pipeline de integração contínua</li>
     <li>Criar uma documentação mínima para seu projeto de testes automatizados</li>
   </ul>
-  `
-};
+  `);
+}
 
-
-
-
-  // <br>
-  // <li>Introdução à Qualidade</li>
-  // <li>Cultura da Qualidade</li>
-  // <li>Metodologia Ágil</li>
-  // <li>Gestão de projetos</li>
-  // <li>Teste de Software</li>
-  // <li>MongoDB</li>
-  // <li>Postman</li>
+// <br>
+// <li>Introdução à Qualidade</li>
+// <li>Cultura da Qualidade</li>
+// <li>Metodologia Ágil</li>
+// <li>Gestão de projetos</li>
+// <li>Teste de Software</li>
+// <li>MongoDB</li>
+// <li>Postman</li>
